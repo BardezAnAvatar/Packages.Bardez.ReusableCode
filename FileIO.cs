b5cbb9cfe4e6f05e8e3d861bbbad79b6e193ae53
Erik "Bardez" Frantz <bardez@bardez.net>
2010-07-13 04:01:27 +0000

    Add the Infinity +1 Solution to TFS
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C5

---
+++ FileIO.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace InfinityPlus1.ReusableCode
+{
+    /// <summary>This public class is just a collection of static methods that can be reused from many different code locations.</summary>
+    public static class ReusableIO
+    {
+        #region Public Methods
+        /// <summary>This public method reads</summary>
+        /// <param name="Input">Stream to read from</param>
+        /// <param name="ReadLength">Length of binary data to read</param>
+        /// <returns>a Byte array containing the read data</returns>
+        public static Byte[] BinaryRead(Stream Input, Int32 ReadLength)
+        {
+            Int32 offset = 0;
+            Int32 remainingLength = ReadLength;
+            Byte[] returnArr = new Byte[ReadLength];
+
+            //read repeatedly from the stream. this accounts for reading that stops before eof, if the stream is buffered by a slow drive of network, etc.
+            while (remainingLength > 0)
+            {
+                //do the read
+                Int32 readCount = Input.Read(returnArr, offset, remainingLength);
+
+                //if there was a problem with the read
+                if (readCount < 1)
+                    throw new EndOfStreamException(String.Format("End of string reached with {0} bytes left to read", remainingLength));
+
+                remainingLength -= readCount;
+                offset += readCount;
+            }
+
+            return returnArr;
+        }
+
+        /// <summary>This public method reads an Int16 from the source array</summary>
+        /// <param name="Source">The source array to read from</param>
+        /// <param name="Offset">The offset within the source array to read from</param>
+        /// <returns>The Int16 read from the array</returns>
+        public static Int16 ReadInt16FromArray(ref Byte[] Source, Int32 Offset)
+        {
+            Byte[] buffer = ReadVariableFromArray(ref Source, 2, Offset);
+            return BitConverter.ToInt16(buffer, 0);
+        }
+
+        /// <summary>This public method reads an Int32 from the source array</summary>
+        /// <param name="Source">The source array to read from</param>
+        /// <param name="Offset">The offset within the source array to read from</param>
+        /// <returns>The Int32 read from the array</returns>
+        public static Int32 ReadInt32FromArray(ref Byte[] Source, Int32 Offset)
+        {
+            Byte[] buffer = ReadVariableFromArray(ref Source, 4, Offset);
+            return BitConverter.ToInt32(buffer, 0);
+        }
+        
+        /// <summary>This public method reads an Int64 from the source array</summary>
+        /// <param name="Source">The source array to read from</param>
+        /// <param name="Offset">The offset within the source array to read from</param>
+        /// <returns>The Int64 read from the array</returns>
+        public static Int64 ReadInt64FromArray(ref Byte[] Source, Int32 Offset)
+        {
+            Byte[] buffer = ReadVariableFromArray(ref Source, 8, Offset);
+            return BitConverter.ToInt64(buffer, 0);
+        }
+
+        /// <summary>This public method reads a string from a byte array of an ASCII-encoding</summary>
+        /// <param name="Source">Byte array to read from</param>
+        /// <param name="Offset">Offset within the byte array to read from</param>
+        /// <param name="Length">Optional parameter indicating the length of the string to read. The default value is 8, for resource references.</param>
+        /// <returns>The string read from the byte array</returns>
+        public static String ReadStringFromByteArray(ref Byte[] Source, Int32 Offset, String CultureRef, Int32 Length = 8)
+        {
+            Byte[] temp = new Byte[Length];
+            Array.Copy(Source, Offset, temp, 0, Length);
+
+            CultureInfo culture = new CultureInfo(CultureRef);
+            Encoding encoding = Encoding.GetEncoding(culture.TextInfo.ANSICodePage);
+            return encoding.GetString(temp);
+        }
+        #endregion
+
+        #region Private Helper Methods
+        /// <summary>
+        ///     This private method will read binary data of a given length from an array and return that sub-array.
+        ///     If the system is Big-Endian, the resultant array will be flipped.
+        /// </summary>
+        /// <param name="Source">The original data array</param>
+        /// <param name="Length">The length of binary data to be extracted</param>
+        /// <param name="Offset">The offset within the source array to start reading at</param>
+        /// <returns>a Byte array containing the extracted data</returns>
+        private static Byte[] ReadVariableFromArray(ref Byte[] Source, Int32 Length, Int32 Offset)
+        {
+            Byte[] buffer = new Byte[Length];
+            Array.Copy(Source, Offset, buffer, 0, Length);
+
+            //This is stupid, but I am eyeing the iPhone... it makes me *aware* of such issues
+            if (! BitConverter.IsLittleEndian)
+                Array.Reverse(buffer);
+
+            return buffer;
+        }
+        #endregion
+    }
+}
