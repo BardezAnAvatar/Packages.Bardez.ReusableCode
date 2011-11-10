a655cb4fc97d4807e300727ebcb8e4d8480afd78
Erik "Bardez" Frantz <bardez@bardez.net>
2011-11-10 06:00:57 +0000

    Moved to Reusable Code since enums have wider utility than just the Infinity Engine file formats
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C62

---
+++ EnumDescriptionToString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Bardez.Projects.InfinityPlus1.Files.Infinity.Common.Enums
+{
+    /// <summary>Enumerator extension method static class</summary>
+    public static class EnumDescriptionToString
+    {
+        /// <summary>Extension method that wil genrate a String from an enum's Description Attribute</summary>
+        /// <param name="me">Enum extended</param>
+        /// <returns>A String containing either the Description string or the enumerator's ToString() value</returns>
+        public static String GetDescription(this Enum me)
+        {
+            String description = null;
+            
+            object[] attributes = me.GetType().GetField(me.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            if (attributes != null && attributes.Length > 0)
+                description = (attributes[0] as DescriptionAttribute).Description;
+            else
+                description = me.ToString();
+
+            return description;
+        }
+    }
+}
